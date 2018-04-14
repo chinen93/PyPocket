3e0eb6e36b9f25985192c640a651b1f43e4aba77
Pedro Chinen <chinen93@gmail.com>
2018-04-14 17:00:45 -0300

    Make authentication work
    Finish all the steps to authenticate the pocket login

--- GetAuthentication.py
+++ GetAuthentication.py
@@ -1,8 +1,10 @@
 #
 # IMPORTS
 #
-import requests
 import configuration
+import requests
+import webbrowser
+
 
 #
 # CONSTANTS
@@ -11,7 +13,9 @@ CONSUMER_KEY = ""
 HEADERS = {"content-type": "application/json; charset=UTF-8",
            "X-Accept": "application/json"}
 REDIRECT_URL = "https://www.google.com"
-URL = "https://getpocket.com/v3/oauth/request"
+URL_AUTHORIZE = "https://getpocket.com/v3/oauth/authorize"
+URL_REQUEST = "https://getpocket.com/v3/oauth/request"
+URL_LOGIN =  "https://getpocket.com/auth/authorize"
 
 #
 # CODE
@@ -27,16 +31,48 @@ def authenticate(consumerKey):
 
     # try: Get the response from the server.
     try:
-        response = requests.post(URL, headers=HEADERS, json=data)
+        response = requests.post(URL_REQUEST, headers=HEADERS, json=data)
     
     # Error: Print error message and leave.
     except:
-        print("Problem to authenticate")
+        print("Problem to Request")
         exit
     
-    # Save authentication key
-    authentication_key = response.json()["code"]
-    saveKeys(consumerKey, authentication_key)
+    # Get request token
+    request_token = response.json()["code"]
+
+    # Create the url to authorize the pocket account
+    url = URL_LOGIN
+    url += "?request_token=" + request_token
+    url += "&redirect_uri=" + REDIRECT_URL
+
+    # Open the URL in the browser
+    webbrowser.open(url)
+
+    # Get the request token from the callback
+    request_token_callback = response.json()["code"]
+
+    # Data with consumer key and request token
+    data = {"consumer_key": consumerKey,
+            "code": request_token_callback}
+
+    # try: Convert the request token into a pocket access token
+    try:
+        response = requests.post(URL_AUTHORIZE, headers=HEADERS, json=data)
+    
+    # Error: Print error message and leave.
+    except:
+        print("Problem to Authorize")
+        exit
+    
+    # Get authentication key
+    authenticationKey = response.json()["access_token"]
+
+    # Save it into a file
+    saveKeys(consumerKey, authenticationKey)
+    
+    # return the authentication key
+    return authenticationKey
 # authenticate()
 
 
@@ -53,19 +89,19 @@ def getKeys():
         
         # Close file
         fo.close()
+
+        # Remove special characters
+        line = line.replace("\n", "")
+        
+        # Split keys
+        keys = line.split(";")
+        consumerKey = keys[0]
+        authenticateKey = keys[1]
         
     # Can not split keys
     except:
         consumerKey = input("Enter the consumer key: ")
         authenticateKey = None
-
-    # Remove special characters
-    line = line.replace("\n", "")
-
-    # Split keys
-    keys = line.split(";")
-    consumerKey = keys[0]
-    authenticateKey = keys[1]
 
     # return keys
     return consumerKey, authenticateKey
@@ -83,7 +119,7 @@ def main():
     # If authentication key does not exist: Authenticate consumer key
     if authenticateKey is None:
         print("Authenticating consumer key")
-        authenticate(consumerKey)
+        authenticateKey = authenticate(consumerKey)
 
     # End program
     print(consumerKey, authenticateKey)
