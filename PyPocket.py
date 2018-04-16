328b58cfd0c659b13b193ad109f32d414373e10b
Pedro Chinen <chinen93@gmail.com>
2018-04-16 08:46:38 -0300

    Add all the parameters and fields that can be used to retrieve itens

--- PyPocket.py
+++ PyPocket.py
@@ -28,18 +28,18 @@ def getKeys():
     """
     I get the consumer and authentication keys from a file
     """
-    
+
     try:
         # Open file to read
         fo = open(configuration.FILE, "r")
         line = fo.readline()
-        
+
         # Close file
         fo.close()
-        
+
     # Can not split keys
     except:
-        print("Keys couldn't be found. Try creating them with GetAuthentication.py")
+        print("Keys couldn't be found. Create them with GetAuthentication.py")
         exit
 
     # Remove special characters
@@ -55,29 +55,40 @@ def getKeys():
 # getKeys()
 
 
-def getJsonPockets(consumerKey, authenticateKey, tags = None, state = None):
+def getJsonPockets(consumerKey, authenticateKey, param):
     """
     I retrieve the pockets saved
     """
 
     # Data to pass to server.
-    data = {"consumer_key": consumerKey, 
-            "access_token": authenticateKey,
-            "detailType": "complete",
-            "sort":"title"}
+    data = {"consumer_key": consumerKey,
+            "access_token": authenticateKey}
 
-    # If exist a tag of itens to be retrieve
-    if(tags):
-        data["tag"] = tags
+    # Add parameters 
+    if(param["contentType"]):
+        data["contentType"] = param["contentType"]
+    if(param["count"]):
+        data["count"] = param["count"]
+    if(param["detailType"]):
+        data["detailType"] = param["detailType"]
+    if(param["favorite"]):
+        data["favorite"] = param["favorite"]
+    if(param["state"]):
+        data["state"] = param["state"]
+    if(param["sort"]):
+        data["sort"] = param["sort"]
+    if(param["search"]):
+        data["search"] = param["search"]
+    if(param["tag"]):
+        data["tag"] = param["tag"]
 
-    # Check the state of each 
-    if(state):
-        data["state"] = state
+    if(param["since"]):
+        data["since"] = param["since"]
 
     # try: Get the response from the server.
     try:
         response = requests.post(URL_RETRIEVE, headers=HEADERS, json=data)
-    
+
     # Error: Print error message and leave.
     except:
         print("Problem to authenticate")
@@ -88,7 +99,7 @@ def getJsonPockets(consumerKey, authenticateKey, tags = None, state = None):
 
     # Create a list with only the things that are needed
     items = []
-    
+
     # Foreach item from pocket items
     for pocket_item in pocket_items:
 
@@ -99,10 +110,22 @@ def getJsonPockets(consumerKey, authenticateKey, tags = None, state = None):
         # Try to get descriptions of each item
         try:
             # Put only the descriptions that are relevant
-            item["item_id"] = details["item_id"]
+            # item["authors"] = details["authors"]
+            # item["excerpt"] = details["excerpt"]
+            # item["favorite"] = details["favorite"]
             item["given_title"] = details['given_title']
+            # item["given_url"] = details["given_url"]
+            # item["has_image"] = details["has_image"]
+            # item["has_video"] = details["has_video"]
+            # item["images"] = details["images"]
+            # item["is_article"] = details["is_article"]
+            item["item_id"] = details["item_id"]
+            # item["resolved_id"] = details["resolved_id"]
             item["resolved_title"] = details['resolved_title']
             item["resolved_url"] = details['resolved_url']
+            # item["status"] = details["status"]
+            # item["videos"] = details["videos"]
+            # item["word_count"] = details["word_count"]
             pocket_tags = details['tags']
 
         except KeyError:
@@ -112,7 +135,7 @@ def getJsonPockets(consumerKey, authenticateKey, tags = None, state = None):
         item["tags"] = []
         for tag in pocket_tags:
             item["tags"].append(tag)
-        
+
 
         # Put the item with only the needed descriptions on the return list
         items.append(item)
@@ -127,11 +150,24 @@ def main():
     """
     consumerKey, authenticateKey = getKeys()
 
-    items = getJsonPockets(consumerKey, 
-                           authenticateKey, 
-                           configuration.POCKET_TAG, 
-                           configuration.POCKET_STATE)
-    
+
+    # Parameters for retrieving itens from pocket
+    retrieveParam = {}
+    retrieveParam["contentType"] = None
+    retrieveParam["count"] = None
+    retrieveParam["detailType"] = "complete"
+    retrieveParam["favorite"] = None
+    retrieveParam["search"] = None
+    retrieveParam["sort"] = "title"
+    retrieveParam["state"] = configuration.POCKET_STATE
+    retrieveParam["tag"] = configuration.POCKET_TAG
+
+    retrieveParam["since"] = None
+
+    items = getJsonPockets(consumerKey,
+                           authenticateKey,
+                           retrieveParam)
+
     print(items)
 # main()
 
